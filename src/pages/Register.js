2ba9fba2b0755b6976c59e151e521be40c575d3d
Shivam <badshahshivam@Shivams-MacBook-Air.local>
2026-05-01 11:15:52 +0530

    final full code

---
+++ src/pages/Register.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    try {
+      await axios.post("http://localhost:8080/users/register", {
+        name,
+        email,
+        password,
+        role: "USER",
+      });
+
+      alert("Registered Successfully");
+      window.location.href = "/";
+    } catch (err) {
+      alert("Error");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+}
+
+export default Register;
